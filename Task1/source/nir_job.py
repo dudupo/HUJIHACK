e98c8d61ec5d6216f99add3211ad3d34b93e58f4
nir.vaknin <nir.vaknin@mail.huji.ac.il>
2020-06-11 17:31:21 +0300

    test

--- Task1/source/nir_job.py
+++ Task1/source/nir_job.py
@@ -4,9 +4,8 @@ from models import *
 import time
 from strong import pre_proc, categorical, featuers, droped_fe
 
-
-list_classifiers = [ DecisionTree, Logistic, KNearestNeighbor,
-                    RidgeClassifier, LassoClassifier] # LDA,
+list_classifiers = [DecisionTree, Logistic, KNearestNeighbor,
+                    RidgeClassifier, LassoClassifier]  # LDA,
 
 
 # def calculate_classifier(data):
@@ -41,36 +40,39 @@ list_classifiers = [ DecisionTree, Logistic, KNearestNeighbor,
 #     return dictionary
 
 
-
 def calculate_classifier(data):
     data1 = data["CRSArrTime"].to_numpy()
     data2 = data["CRSDepTime"].to_numpy()
     data3 = data["CRSElapsedTime"].to_numpy()
     data4 = data["Distance"].to_numpy()
     data5 = data["DayOfWeek"].to_numpy()
-    data_cols = [("CRSArrTime", data1), ("CRSDepTime", data2),
-                 ("CRSElapsedTime", data3), ("Distance", data4),
-                 ("DayOfWeek", data5)]
-    y = data["ArrDelay"].to_numpy()
+
+
+    X, y = pre_proc(data, [], categorical)
+    data_cols = [ (key, np.array(X[key]) ) for key in X.keys() ]
+    # ("CRSArrTime", data1), ("CRSDepTime", data2),
+    # ("CRSE)lapsedTime", data3), ("Distance", data4),
+    # ("DayOfWeek", data5)]
     dictionary = {"CRSArrTime": [], "CRSDepTime": [], "CRSElapsedTime": [],
                   "Distance": [], "DayOfWeek": []}
-
-    X, y = pre_proc(data ,droped_fe, categorical)
+    print(X.columns)
     for col in data_cols:
         for WeekLearner in list_classifiers:
-            X = X[col[0]].to_numpy()
-            print(X)
-            print(y)
+            print(X.shape)
+            print(y.shape)
             # X = X.reshape(X.shape[0],)
-
             classifier = WeekLearner()
             start = time.time()
-            classifier.fit(X, y)
+            if isinstance(classifier, Logistic):
+                classifier.fit(np.array([col[1]]).T.flatten(), y)
+            else:
+                classifier.fit(np.array([col[1]]).T, y)
             end = time.time()
             time_elapses = end - start
             score = classifier.score(X, y)
             dictionary[col[0]].append((WeekLearner, score, time_elapses))
     return dictionary
+
 
 if __name__ == '__main__':
     path_csv = "../train_data.csv"
