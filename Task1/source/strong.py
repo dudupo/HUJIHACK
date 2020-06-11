35a7bf93284898148ef4adf82a0672369a1903e4
david ponarovsky <dudupo51@gmail.com>
2020-06-11 10:02:49 +0300

    debugging stage

--- Task1/source/strong.py
+++ Task1/source/strong.py
@@ -53,8 +53,9 @@ def generateTeamClass(featuers):
             self.featuers = featuers
 
         def filterX( self, X ):
-            
-            return [ X[featuer] for featuer in self.featuers ]
+            ret = pd.DataFrame( { featuer:  X[featuer] for featuer in self.featuers  }).to_numpy()
+            print(ret)
+            return ret
 
         def train(self, X, y):
             super().fit(
@@ -64,30 +65,13 @@ def generateTeamClass(featuers):
         
     return WeakTeam
 
-featuers =   ["DayOfWeek",
-            "FlightDate",
-            "Reporting_Airline",
-            "Tail_Number",
-            "Flight_Number_Reporting_Airline",
-            "Origin",
-            "OriginCityName",
-            "OriginState",
-            "Dest",
-            "DestCityName",
-            "DestState",
-            "CRSDepTime",
-            "CRSArrTime",
-            "CRSElapsedTime",
-            "Distance"]
-            
-            # "ArrDelay",
-            # "DelayFactor"]
 
 
 
-def learn(_dataframe, y):
-    agents = 42
-    group_size = 4
+
+def learn(_dataframe, y, featuers ):
+    agents = 3
+    group_size = 2
     subgroups = [ generateTeamClass(team) for team in combinations(featuers, group_size) ]
 
     def calc_error(model, _dataframe, y):
@@ -111,15 +95,70 @@ if __name__ == "__main__" :
     # cat = pd.DataFrame ( { 'id' : _dataset_prepoc['id'] } , pd.get_dummies(_dataset_prepoc[categorical].astype('category') ))
     
     
+    def pre_proc(_dataset, droped_fe, categorical ):
+
+        def generateY(_dataset):
+            y = []
+            for _bool in _dataset["ArrDelay"] > 0 :
+                y.append( {  False : [0] , True : [1]  }[ _bool ] )
+            return np.array( y )
+
+        # def remove_end_cases(_frame):
+        #     return _frame[  _frame['price'] > 0  ]
+        y = generateY(_dataset) 
+        
+        cat = pd.DataFrame(  pd.get_dummies(_dataset[categorical].astype('category') ))
+        _dataset = _dataset.drop( droped_fe + categorical, axis=1)
+        _dataset_prepoc = pd.concat([ _dataset.reset_index(drop=True), cat.reset_index(drop=True)], axis=1)
+        return _dataset_prepoc, y
+
+
+    featuers =   ["DayOfWeek",
+            "FlightDate",
+            "Reporting_Airline",
+            "Tail_Number",
+            "Flight_Number_Reporting_Airline",
+            "Origin",
+            "OriginCityName",
+            "OriginState",
+            "Dest",
+            "DestCityName",
+            "DestState",
+            "CRSDepTime",
+            "CRSArrTime",
+            "CRSElapsedTime",
+            "Distance"]
+    
+    droped_fe = [ 'Flight_Number_Reporting_Airline',
+                 'Tail_Number',
+                 'FlightDate',
+                 'ArrDelay' ,
+                 'DelayFactor' ]
+    
+    categorical = [
+        'OriginCityName' 
+        , 'OriginState'
+        , 'Origin'
+        , 'Dest'
+        , 'DestCityName'
+        , 'DestState'
+        , 'Reporting_Airline']
+
+            # "ArrDelay",
+            # "DelayFactor"]
+
+    print("[#] before pre processing")
     print(_dataset)
-    _dataset.drop( ["FlightDate"] )
-    y = []
-    for _bool in _dataset["ArrDelay"] > 0 :
-        y.append( {  False : 0 , True : 1  }[ _bool ] )
-    print(np.array(y))
-
-    _mod = learn(_dataset, np.array(y))
-
+    _dataset, y  = pre_proc(_dataset, droped_fe, categorical )
+    print("[#] after pre processing")
+    print(_dataset)
+    print("[#] y' vector ")
+    print(y)
+    
+    print( _dataset.keys())
+    _mod = learn(_dataset, y, _dataset.keys() )
+    print("[#] best featuers:")
+    print(_mod.featuers)
 
 
 
