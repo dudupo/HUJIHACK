1c1b886b8e19c27b3e75fc687b5b1c32149e1fe7
nir.vaknin <nir.vaknin@mail.huji.ac.il>
2020-06-11 15:41:34 +0300

    test

--- Task1/source/nir_job.py
+++ Task1/source/nir_job.py
@@ -9,19 +9,51 @@ list_classifiers = [ Logistic, DecisionTree, KNearestNeighbor,
                     RidgeClassifier, LassoClassifier] # LDA,
 
 
+# def calculate_classifier(data):
+#     data1 = data.as_matrix(columns=["CRSArrTime"])
+#     data2 = data.as_matrix(columns=["CRSDepTime"])
+#     data3 = data.as_matrix(columns=["CRSElapsedTime"])
+#     data4 = data.as_matrix(columns=["Distance"])
+#     data5 = data.as_matrix(columns=["DayOfWeek"])
+#     data_cols = [("CRSArrTime", data1), ("CRSDepTime", data2),
+#                  ("CRSElapsedTime", data3), ("Distance", data4),
+#                  ("DayOfWeek", data5)]
+#     y = data.as_matrix(columns=["ArrDelay"])
+#     dictionary = {"CRSArrTime": [], "CRSDepTime": [], "CRSElapsedTime": [],
+#                   "Distance": [], "DayOfWeek": []}
+#
+#
+#     X, y = pre_proc(data ,droped_fe, categorical )
+#     for col in data_cols:
+#         for WeekLearner in list_classifiers:
+#             # X = col[1]
+#             print(X)
+#             print(y)
+#             # X = X.reshape(X.shape[0],)
+#
+#             classifier = WeekLearner()
+#             start = time.time()
+#             classifier.fit(X, y)
+#             end = time.time()
+#             time_elapses = end - start
+#             score = classifier.score(X, y)
+#             dictionary[col[0]].append((WeekLearner, score, time_elapses))
+#     return dictionary
+
+
+
 def calculate_classifier(data):
-    data1 = data.as_matrix(columns=["CRSArrTime"])
-    data2 = data.as_matrix(columns=["CRSDepTime"])
-    data3 = data.as_matrix(columns=["CRSElapsedTime"])
-    data4 = data.as_matrix(columns=["Distance"])
-    data5 = data.as_matrix(columns=["DayOfWeek"])
+    data1 = data["CRSArrTime"].to_numpy()
+    data2 = data["CRSDepTime"].to_numpy()
+    data3 = data["CRSElapsedTime"].to_numpy()
+    data4 = data["Distance"].to_numpy()
+    data5 = data["DayOfWeek"].to_numpy()
     data_cols = [("CRSArrTime", data1), ("CRSDepTime", data2),
                  ("CRSElapsedTime", data3), ("Distance", data4),
                  ("DayOfWeek", data5)]
-    y = data.as_matrix(columns=["ArrDelay"])
+    y = data["ArrDelay"].to_numpy()
     dictionary = {"CRSArrTime": [], "CRSDepTime": [], "CRSElapsedTime": [],
                   "Distance": [], "DayOfWeek": []}
-
 
     X, y = pre_proc(data ,droped_fe, categorical )
     for col in data_cols:
@@ -40,10 +72,10 @@ def calculate_classifier(data):
             dictionary[col[0]].append((WeekLearner, score, time_elapses))
     return dictionary
 
-
 if __name__ == '__main__':
     path_csv = "../train_data.csv"
     df = pd.read_csv(path_csv)
     df.dropna(inplace=True)
+    df = df.head(30)
     calculate_classifier(df)
     print(df.columns)
