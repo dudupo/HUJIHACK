c1ce9bb58e51bfce838c309cc084e069054d97af
Nir Vaknin <nir.vaknin@cs.huji.ac.il>
2020-06-12 03:21:13 +0300

    Merge branch 'master' of https://github.com/dudupo/HUJIHACK
    # Conflicts:
    #	Task1/source/nir_job.py

--- Task1/source/strong.py
+++ Task1/source/strong.py
@@ -7,9 +7,9 @@ from binarysearch import binarysearch
 from datetime import date
 from random import shuffle
 
-
 from classifier import final_pre_proc
 from sklearn.model_selection import train_test_split
+
 
 # class WeakFactory:
 #     class WeakLernerByFeature(abcModel):
@@ -23,10 +23,10 @@ from sklearn.model_selection import train_test_split
 #         def predict(self, X):
 #             return self.mod.predict()
 
-    
+
 #     def __init__ (self):
 #         pass
-    
+
 #     @staticmethod
 #     def CreateWeaks(self):
 
@@ -59,25 +59,27 @@ def generateTeamClass(featuers):
                 Type.__init__(self)
                 self.featuers = featuers
 
-            def filterX( self, X ):
-                ret = np.array( pd.DataFrame( { featuer:  X[featuer] for featuer in self.featuers  }))
+            def filterX(self, X):
+                ret = np.array(pd.DataFrame(
+                    {featuer: X[featuer] for featuer in self.featuers}))
                 return ret
 
             def train(self, X, y, D=None):
-                #print(f"[@] train on features : { self.featuers}")
-                
+                # print(f"[@] train on features : { self.featuers}")
+
                 if D is None:
                     super().fit(
-                        np.array(self.filterX(X)),  y)
+                        np.array(self.filterX(X)), y)
                 else:
                     super().fit(
                         np.array(self.filterX(X)), y, D)
-            
+
             def predict(self, X):
-                return super().predict( self.filterX(X) )
-        
+                return super().predict(self.filterX(X))
+
         return WeakTeam
-    #return generateWeakClass( DecisionTree ) 
+
+    # return generateWeakClass( DecisionTree )
     # return generateWeakClass( Logistic )
 
     return generateWeakClass(DecisionStumpWarper)
@@ -85,10 +87,10 @@ def generateTeamClass(featuers):
 
 def calc_error(model, _dataframe, y, agents):
     _error = 0
-    z = (y.flatten() - 0.5 * np.ones(len(y)))*2
-    for _bool in (model.predict(_dataframe, max_t=agents ) != z ): 
-        _error += {  False : 0 , True : 1  }[ _bool ]
-    return _error / len( y ) 
+    z = (y.flatten() - 0.5 * np.ones(len(y))) * 2
+    for _bool in (model.predict(_dataframe, max_t=agents) != z):
+        _error += {False: 0, True: 1}[_bool]
+    return _error / len(y)
 
 
 import heapq
@@ -100,108 +102,174 @@ def hash_strings(featuers, _list):
     for featuer in featuers:
         if featuer in _list:
             ret += base
-        base*= 10
-    return ret 
+        base *= 10
+    return ret
 
-def learn(_dataframe, y, featuers, teams= set(), depth = 5, orignal=[] , _hased = set()):
 
+def learn(_dataframe, y, featuers, teams=set(), depth=5, orignal=[],
+          _hased=set()):
     if len(teams) == 0:
-        teams = [ [featuer] for  featuer in featuers  ]
+        teams = [[featuer] for featuer in featuers]
 
     agents = 42
     group_size = 2
     _hashed = set()
     new_team = []
+
     def create_subgroups():
-        subgroups  = []
+        subgroups = []
         for featuer in featuers:
             for team in teams:
                 if featuer not in team:
-                    _hash = hash_strings(orignal, team + [featuer] ) 
+                    _hash = hash_strings(orignal, team + [featuer])
                     if _hash not in _hashed:
                         # print(f"i was here {team + [featuer]}, _hash : {_hash}")
-                        subgroups.append( generateTeamClass( team + [featuer] ) )
+                        subgroups.append(generateTeamClass(team + [featuer]))
                         _hashed.add(_hash)
         return subgroups
-    
+
     strongGroups = []
-    heap = [ ]
+    heap = []
 
     for weak in create_subgroups():
         _model = AdaBoost(weak, agents, support_wights=True)
         _model.train(_dataframe, y)
-        strongGroups.append( _model )
-        heapq.heappush( heap,  (-calc_error( _model, _dataframe, y, agents ) , _model.h[0].featuers, _model  ) )
+        strongGroups.append(_model)
+        heapq.heappush(heap, (
+            -calc_error(_model, _dataframe, y, agents), _model.h[0].featuers,
+            _model))
 
         if len(heap) > 30000:
-            heapq.heappop( heap )
+            heapq.heappop(heap)
 
     if depth == 0:
         while len(heap) > 1:
-            train_error, _featuers, _model =  heapq.heappop( heap )
-            #print(train_error)
-        train_error, _featuers, _model =  heapq.heappop( heap )
+            train_error, _featuers, _model = heapq.heappop(heap)
+            # print(train_error)
+        train_error, _featuers, _model = heapq.heappop(heap)
         return train_error, _featuers, _model
-    
+
     else:
         teams = []
         while len(heap) > 1:
-            heapq.heappop( heap )
-            train_error, _featuers, _model =  heapq.heappop( heap )
-            teams.append( _featuers  )
-        #print(teams)
+            heapq.heappop(heap)
+            train_error, _featuers, _model = heapq.heappop(heap)
+            teams.append(_featuers)
+        # print(teams)
         return learn(_dataframe, y, featuers, teams, depth - 1,
-         orignal=orignal )
+                     orignal=orignal)
 
 
 def generateY(_dataset, treshold=0):
-        y = []
-        for _bool in _dataset["ArrDelay"] > treshold:
-            y.append( {  False : [0] , True : [1]  }[ _bool ] )
-        return np.array( y )
+    y = []
+    for _bool in _dataset["ArrDelay"] > treshold:
+        y.append({False: [0], True: [1]}[_bool])
+    return np.array(y)
 
-def pre_proc(_dataset, droped_fe, categorical ):
 
+def generateYbyString(_dataset, s, treshold=0):
+    y = []
+    for _bool in _dataset[s] > treshold:
+        y.append({False: [0], True: [1]}[_bool])
+    return np.array(y)
+
+
+def pairs():
+    df1 = df_copy.loc[np.logical_or(df_copy["DelayFactor"] == 0,
+                                    df_copy["DelayFactor"] == 1)]
+    df2 = df1.drop(columns=["DelayFactor"])
+    learn(df2, df1["DelayFactor"].as_matrix(), df2.keys(), orignal=df2.keys())
+
+    df1 = df_copy.loc[np.logical_or(df_copy["DelayFactor"] == 1,
+                                    df_copy["DelayFactor"] == 2)]
+    df2 = df1.drop(columns=["DelayFactor"])
+    learn(df2, df1["DelayFactor"].as_matrix() - 1, df2.keys(),
+          orignal=df2.keys())
+
+    df1 = df_copy.loc[np.logical_or(df_copy["DelayFactor"] == 2,
+                                    df_copy["DelayFactor"] == 3)]
+    df2 = df1.drop(columns=["DelayFactor"])
+    learn(df2, df1["DelayFactor"].as_matrix() - 2, df2.keys(),
+          orignal=df2.keys())
+
+    df1 = df_copy.loc[np.logical_or(df_copy["DelayFactor"] == 3,
+                                    df_copy["DelayFactor"] == 1)]
+    df2 = df1.drop(columns=["DelayFactor"])
+    s = df1["DelayFactor"].as_matrix()
+    s[s == 3] = 0
+    df1 = df1.drop(columns=["DelayFactor"])
+    df1["DelayFactor"] = s
+    learn(df2, df1["DelayFactor"].as_matrix(), df2.keys(),
+          orignal=df2.keys())
+
+
+    df1 = df_copy.loc[np.logical_or(df_copy["DelayFactor"] == 2,
+                                    df_copy["DelayFactor"] == 0)]
+    df2 = df1.drop(columns=["DelayFactor"])
+    s = df1["DelayFactor"].as_matrix()
+    s[s == 2] = 1
+    df1 = df1.drop(columns=["DelayFactor"])
+    df1["DelayFactor"] = s
+    learn(df2, df1["DelayFactor"].as_matrix(), df2.keys(),
+          orignal=df2.keys())
+
+
+    df1 = df_copy.loc[np.logical_or(df_copy["DelayFactor"] == 3,
+                                    df_copy["DelayFactor"] == 0)]
+    df2 = df1.drop(columns=["DelayFactor"])
+    s = df1["DelayFactor"].as_matrix()
+    s[s == 3] = 1
+    df1 = df1.drop(columns=["DelayFactor"])
+    df1["DelayFactor"] = s
+    learn(df2, df1["DelayFactor"].as_matrix(), df2.keys(),
+          orignal=df2.keys())
+
+    return
+
+
+def pre_proc(_dataset, droped_fe, categorical):
     # def remove_end_cases(_frame):
     #     return _frame[  _frame['price'] > 0  ]
     y = generateY(_dataset)
 
-    if len(categorical) > 0 :
-        cat = pd.DataFrame(  pd.get_dummies(_dataset[categorical].astype('category') ))
+    if len(categorical) > 0:
+        cat = pd.DataFrame(
+            pd.get_dummies(_dataset[categorical].astype('category')))
 
-
-    _dataset = _dataset.drop( droped_fe + categorical, axis=1)
-    if len(categorical) > 0 :
-        _dataset_prepoc = pd.concat([ _dataset.reset_index(drop=True), cat.reset_index(drop=True)], axis=1)
-    else :
-        _dataset_prepoc = _dataset 
+    _dataset = _dataset.drop(droped_fe + categorical, axis=1)
+    if len(categorical) > 0:
+        _dataset_prepoc = pd.concat(
+            [_dataset.reset_index(drop=True), cat.reset_index(drop=True)],
+            axis=1)
+    else:
+        _dataset_prepoc = _dataset
 
     print(_dataset_prepoc)
     return _dataset_prepoc, y
 
 
-featuers =   ["DayOfWeek",
-              "FlightDate",
-              "Reporting_Airline",
-              "Tail_Number",
-              "Flight_Number_Reporting_Airline",
-              "Origin",
-              "OriginCityName",
-              "OriginState",
-              "Dest",
-              "DestCityName",
-              "DestState",
-              "CRSDepTime",
-              "CRSArrTime",
-              "CRSElapsedTime",
-              "Distance"]
+featuers = ["DayOfWeek",
+            "FlightDate",
+            "Reporting_Airline",
+            "Tail_Number",
+            "Flight_Number_Reporting_Airline",
+            "Origin",
+            "OriginCityName",
+            "OriginState",
+            "Dest",
+            "DestCityName",
+            "DestState",
+            "CRSDepTime",
+            "CRSArrTime",
+            "CRSElapsedTime",
+            "Distance"]
 
-droped_fe = [ 'Flight_Number_Reporting_Airline',
-              'Tail_Number',
-            #   'DayOfWeek',
-              'FlightDate',
-              'ArrDelay' ,
-              'DelayFactor' ]
+droped_fe = ['Flight_Number_Reporting_Airline',
+             'Tail_Number',
+             #   'DayOfWeek',
+             'FlightDate',
+             'ArrDelay',
+             'DelayFactor']
 
 categorical = [
     'OriginCityName'
@@ -212,9 +280,7 @@ categorical = [
     , 'DestState'
     , 'Reporting_Airline']
 
-
-
-if __name__ == "__main__" :
+if __name__ == "__main__":
     original_dataset = pd.read_csv("~/data/train_data.csv", nrows=7000)
 
     print("[#] before pre processing")
@@ -222,7 +288,7 @@ if __name__ == "__main__" :
     # _dataset, y  = pre_proc(_dataset, droped_fe, categorical )
     print("[#] after pre processing")
     print("[#] y' vector ")
-    
+
     # _mods = learn(_dataset, y, _dataset.keys() )
     # print("[#] best featuers:")
     # print(_mod.h[0].featuers)
@@ -230,60 +296,55 @@ if __name__ == "__main__" :
     '''
         just to check compiletion.  
     '''
-    _mods = {}  
+    _mods = {}
     _minrange, _maxrange = -30, 30
-    #_dataset, y = pre_proc(original_dataset, droped_fe , categorical )
-    _dataset, y, x_factor,y_factor = final_pre_proc(original_dataset)
+    # _dataset, y = pre_proc(original_dataset, droped_fe , categorical )
+    _dataset, y, x_factor, y_factor = final_pre_proc(original_dataset)
 
     print(_dataset)
-    print( y )
+    print(y)
 
+    print("-------------------------------------------------------")
+    print(y)
 
+    df_copy = _dataset.copy()
+    df_copy["DelayFactor"] = y_factor
+    pairs()
 
-    start_range , end_range = np.ones(len(y)) * _minrange , np.ones(len(y)) * _maxrange
-    for i, time in enumerate( np.arange(_minrange, _maxrange,  _maxrange/ 2**5 ) ):
+    start_range, end_range = np.ones(len(y)) * _minrange, np.ones(
+        len(y)) * _maxrange
+    for i, time in enumerate(
+            np.arange(_minrange, _maxrange, _maxrange / 2 ** 5)):
         train_error, _featuers, _mods[time] = learn(_dataset,
-                        generateY(original_dataset, time), 
-                            _dataset.keys(), teams= [  ['CRSDepTime', 'DayOfWeek'] ], orignal=_dataset.keys())  
-        print( f"{time} : {_featuers} : {train_error}")
+                                                    generateY(original_dataset,
+                                                              time),
+                                                    _dataset.keys(), teams=[
+                ['CRSDepTime', 'DayOfWeek']], orignal=_dataset.keys())
+        print(f"{time} : {_featuers} : {train_error}")
 
     # print( i, time)
-    #times_tersholds = { time : _mods[i] for i, time in enumerate( range(0, 1, 2**-4) ) }
+    # times_tersholds = { time : _mods[i] for i, time in enumerate( range(0, 1, 2**-4) ) }
 
-    Bagent = binarysearch( _mods ) 
-    _middles = Bagent.predict( _dataset , start_range , end_range)
+    Bagent = binarysearch(_mods)
+    _middles = Bagent.predict(_dataset, start_range, end_range)
     print(_middles)
 
-    _dataset, y, x_factor,y_factor= final_pre_proc( pd.read_csv("~/data/train_data.csv", nrows=10000 )[9800:] )
+    _dataset, y, x_factor, y_factor = final_pre_proc(
+        pd.read_csv("~/data/train_data.csv", nrows=10000)[9800:])
     # _dataset, y = pre_proc(original_dataset, droped_fe , categorical )
-    _middles = Bagent.mods[0.0].predict( _dataset )
-    _middles[ _middles > 0 ] = 1
-    t = sum( (_middles- y.flatten())**2/len(y))
-    print (  f"[error]: {t}"  )    
-    #print( )
-
-
-
-
-
-
-
-
-
-
-
-
+    _middles = Bagent.mods[0.0].predict(_dataset)
+    _middles[_middles > 0] = 1
+    t = sum((_middles - y.flatten()) ** 2 / len(y))
+    print(f"[error]: {t}")
+    # print( )
 
 # class StrongClassifer:
-    
+
 #     def __init__ (self, features):
 #         pass
 
 #     def predict(self, X):
 #         pass
-    
+
 #     def train(self, X, y):
 #         pass
-
-    
-
